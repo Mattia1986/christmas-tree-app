dc5c58467056ca3c9a7330e54256b176bd9df341
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2016-11-13 11:33:43 +0000

    Add in-memory users

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # christmas-tree-app
 
 https://expressjs.com/
+
 https://expressjs.com/en/starter/generator.html
 
+https://docs.npmjs.com/
+
 npm install
+
 npm start
+
 http://localhost:3000
+
+git tutorial link
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+var users = {};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mattia' });
 });
+
+/* GET /admin/users */
+router.get('/admin/users', function(req, res, next) {
+  res.render('users', { 
+    users: users,
+    title: 'List all users' });
+});
+
+/* GET /register Registration page */
+router.get('/register', function(req, res, next) {
+  res.render('register', {
+    name: '',
+    surname: '',
+    title: 'Register page' });
+});
+
+/* POST /register Registration page */
+router.post('/register', function(req, res, next) {
+  users[req.body.email] = req.body;
+
+  res.render('register', { 
+    title: 'User registration',
+    name: req.body.name,
+    surname: req.body.surname});
+});
+
+
+
 
 module.exports = router;
